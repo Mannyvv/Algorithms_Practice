30a0172ca35c14749b1e8e3e0cb3ee2ef501183b
Manny Vera <vallesvera.manuel@gmail.com>
2023-11-06 02:49:26 -0800

    function to reverse linked list

--- LinkedList.py
+++ LinkedList.py
@@ -8,6 +8,7 @@ class Node():
 class LinkedList():
     def __init__(self):
         self.head = None
+        
     
 
     def add_node(self,new_node):
@@ -15,6 +16,7 @@ class LinkedList():
         #If head is absent, new node becomes head
         if self.head is None:
             self.head = new_node
+            self.list_data()
             return
 
         #if head is present, add node to the latest node
@@ -31,18 +33,17 @@ class LinkedList():
         
         current_node = self.head
         list_of_data = []
-
         while current_node:
             list_of_data.append(current_node.data)
             current_node = current_node.next_node
         print("Data in each node: " , list_of_data)
 
-offical_linked_list = LinkedList()
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
+# offical_linked_list = LinkedList()
+# node1 = Node(1)
+# node2 = Node(2)
+# node3 = Node(3)
 
-offical_linked_list.add_node(node1)
-offical_linked_list.add_node(node2)
-offical_linked_list.add_node(node3)
+# offical_linked_list.add_node(node1)
+# offical_linked_list.add_node(node2)
+# offical_linked_list.add_node(node3)
 
